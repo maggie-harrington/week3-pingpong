5f39132d6e81e1ea9e75c640470234dbb09f4a26
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-20 11:51:45 -0800

    add alerts for non-numeric and non-positive input

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,13 @@ $(document).ready(function(){
   $("form#input-number").submit(function(event){
     enteredNumber = parseInt($("input#entered-number").val());
     console.log(enteredNumber);
-    pingPong(enteredNumber);
+    if (isNaN (enteredNumber)) {
+      alert ("Error: Please enter a numeric value!");
+    } else if (enteredNumber <= 0) {
+      alert ("Error: Please enter a positive number!");
+    } else {
+      pingPong(enteredNumber);
+    }
     console.log(newNumber);
     $("#output").text(newNumber);
     event.preventDefault();
